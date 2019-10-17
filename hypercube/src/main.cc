81f65305c50d32a44cb2542af7b73331629b7cf3
msiampou <sdi1600151@di.uoa.gr>
2019-10-17 03:34:14 +0300

    finished A-NN HyperCube

--- hypercube/src/main.cc
+++ hypercube/src/main.cc
@@ -178,6 +178,7 @@ int main(int argc, char **argv) {
 
   /* Creating LSH class object and a vector to store approx-NN results */
   start = high_resolution_clock::now();
+  std::vector<std::tuple<T,U,double>> cube_nn_results(input_info.Q);
   std::cout << "\nBuilding HyperCube.." << std::endl;
   std::vector<std::tuple<T,U,double>> hypercube_nn_results(input_info.Q);
   search::HyperCube<T,U> cube{input_info.k, input_info.M, input_info.D,
@@ -188,16 +189,27 @@ int main(int argc, char **argv) {
   std::cout << "Building HyperCube completed successfully." << std::endl;
   std::cout << "Time elapsed: " << total_time.count() << " seconds" << std::endl;
 
-  // /* Executing approximate Nearest Neighbor using LSH */
-  // start = high_resolution_clock::now();
-  // std::cout << "\nExecuting Nearest Neighbor using HyperCube.." << std::endl;
-  // for (int i = 0; i < input_info.Q; ++i) {
-  //   //hypercube_results[i] = hypercube.NearestNeighbor(query_points, i);
-  // }
-  // stop = high_resolution_clock::now();
-  // total_time = duration_cast<duration<double>>(stop - start);
-  // std::cout << "Executing Nearest Neighbor using LSH completed successfully." << std::endl;
-  // std::cout << "Time elapsed: " << total_time.count() << " seconds" << std::endl;  
+  /* Executing approximate Nearest Neighbor using LSH */
+  start = high_resolution_clock::now();
+  std::cout << "\nExecuting Nearest Neighbor using HyperCube.." << std::endl;
+  for (int i = 0; i < input_info.Q; ++i) {
+    cube_nn_results[i] = cube.NearestNeighbor(query_points, i);
+  }
+  stop = high_resolution_clock::now();
+  total_time = duration_cast<duration<double>>(stop - start);
+  std::cout << "Executing Nearest Neighbor using LSH completed successfully." << std::endl;
+  std::cout << "Time elapsed: " << total_time.count() << " seconds" << std::endl;  
+
+  /* Compute Max and Average ratio lsh_nn_results / bf_nn_results */
+  start = high_resolution_clock::now();
+  std::cout << "\nCalculating evaluation metric.." << std::endl;
+  std::pair<double,double> metric_res = metric::EvaluationMetric(bf_nn_results, cube_nn_results);
+  stop = high_resolution_clock::now();
+  total_time = duration_cast<duration<double>>(stop - start);
+  std::cout << "Calculating evaluation metric completed successfully." << std::endl;
+  std::cout << "Time elapsed: " << total_time.count() << " seconds" << std::endl;
+  std::cout << "\nMax Af: " << std::get<0>(metric_res) << std::endl;
+  std::cout << "Average Af: " << std::get<1>(metric_res) << std::endl;
 
   return EXIT_SUCCESS;
 }
