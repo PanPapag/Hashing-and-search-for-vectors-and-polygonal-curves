8dd0e6eaf5d9eaef1a90147f86398d00e4a705fb
msiampou <sdi1600151@di.uoa.gr>
2019-10-20 18:00:49 +0300

    reduced memory space

--- curves/headers/vectorization/vectorization.h
+++ curves/headers/vectorization/vectorization.h
@@ -14,45 +14,42 @@ namespace vectorization {
   template <typename T, typename U>
   class Projection  {
     private:
+      int d;
       uint32_t M;
-      // table
+      float eps;
+      size_t K;
     public:
       /* \brief 
           Just a constructor 
       */
       Projection(std::vector<std::pair<T,T>>& dataset_curves, std::vector<int>& dataset_offsets, 
-                std::vector<int>& dataset_lengths, std::vector<U>& dataset_ids) {
+                std::vector<int>& dataset_lengths, std::vector<U>& dataset_ids) : eps(0.5), d(2) {
         // Get max length from all curves and 
         // store all relevant traversals at an M * M array 
-        //M = *max_element(std::begin(dataset_lengths), std::end(dataset_lengths));
-        M = 4; //just for testing
-        //(i,j) = i*M+j
+        M = *max_element(std::begin(dataset_lengths), std::end(dataset_lengths));
+        K = d * (-1) * log(eps) / (eps*eps);
+
+        // Computing relevant traversals
+        // only for upper diagonal cells
+        // to reduce memory space
         size_t size = (M+1)*(M+1);
         std::vector<std::vector<std::pair<int,int>>> relevant_traversals (size);
-        // Storing all neighbors of diagonial cells
         for(size_t i=1; i<M+1; i++) {
-          for(size_t j=1; j<M+1; j++) {
+          for(size_t j=i; j<M+1; j++) {
             size_t max = (i>j) ? i:j;
-            relevant_traversals[i*M+j] = ComputeNeighbors(max);
+            relevant_traversals[i*M+j] = ComputeNeighbors(max, relevant_traversals[i*M+j]);
           }
         }
-        for(size_t i=1; i<M+1; i++)  {
-          for(size_t j=1; j<M+1; j++) {
-            std::cout << i << "," << j << std::endl;
-            std::cout << "-------------" << std::endl;
-            for (auto& i:relevant_traversals[i*M+j]) {
-              std::cout << i.first << " " << i.second << std::endl;
-            }
-            std::cout << std::endl;
-          }
-        }
+        size = K*d*K*d;  
+        std::vector<double> G (size);
+
       };
+
       /* \brief 
           Find neighbor cells of diagonal 
         - these are the relevant traversals of dist 1
       */
-      std::vector<std::pair<int,int>> ComputeNeighbors(int l) {
-        std::vector<std::pair<int,int>> vect;
+      std::vector<std::pair<int,int>>& ComputeNeighbors(int l, std::vector<std::pair<int,int>>& vect) {
         if (l==1) {
           vect.push_back(std::make_pair(1, 1));
           return vect;
